6fd6951c9dd36b8d4ebb54a838f8a5d93d702c91
Nic Ingerson <ingersonmeacham01@allegheny.edu>
2023-01-09 21:22:43 -0500

    added downloading to yt_downloader.py

--- src/yt_downloader.py
+++ src/yt_downloader.py
@@ -3,3 +3,18 @@ from sys import argv
 
 link = argv[1]
 yt = YouTube(link)
+
+print("Title: ", yt.title)
+
+print("Views:", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+print("Downloading...")
+
+# change this value to the path you wish to download to
+path = "/Users/ninge/Videos/YouTube Downloads/"
+
+yd.download(path)
+
+print(f"{yt.title} downloaded to {path}!")
